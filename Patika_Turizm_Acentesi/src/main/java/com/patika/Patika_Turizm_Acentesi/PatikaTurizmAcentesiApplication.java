22a8783c0404c32c39bed17b45c2594be796953c
Suleyman <61500642+suleymanakay@users.noreply.github.com>
2022-07-12 16:16:52 +0300

    Add files via upload

---
+++ Patika_Turizm_Acentesi/src/main/java/com/patika/Patika_Turizm_Acentesi/PatikaTurizmAcentesiApplication.java
@@ -0,0 +1,13 @@
+package com.patika.Patika_Turizm_Acentesi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatikaTurizmAcentesiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatikaTurizmAcentesiApplication.class, args);
+	}
+
+}
---
+++ Patika_Turizm_Acentesi/src/test/java/com/patika/Patika_Turizm_Acentesi/PatikaTurizmAcentesiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patika.Patika_Turizm_Acentesi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatikaTurizmAcentesiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
